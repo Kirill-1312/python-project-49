2a969f31e3e7deb5f8ccb84229d7b091e17ba833
Kirill <kirillbulgakow@mail.ru>
2024-10-29 14:56:20 +0300

    c

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,4 +2,4 @@ from brain_games.engine import engine
 from brain_games.games.game_prime import prime
 
 def main():
-    engine(prime,'Answer "yes" if the number is even, otherwise answer "no".')
+    engine(prime,'Answer "yes" if given number is prime. Otherwise answer "no".')
